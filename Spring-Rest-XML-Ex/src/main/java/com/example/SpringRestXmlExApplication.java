7b19d036bcc3a920eff48370c3f7a9a530348cc9
JAVAWORLD-SAI <saichand1997.s5@gmail.com.com>
2022-07-10 23:13:52 +0530

    Spring Rest XMl Ex

---
+++ Spring-Rest-XML-Ex/src/main/java/com/example/SpringRestXmlExApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestXmlExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestXmlExApplication.class, args);
+	}
+
+}
